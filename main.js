f55f6ac6a1e1b40e801a8f74b6250d601528b962
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-04-29 17:53:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+var nosex;
+var nosey;
+var wristleftX;
+var wristleftY;
+var wristrightX;
+var wristrightY;
 function setup(){
 canvas=createCanvas(600,600)
 canvas.position(600,125)
 video=createCapture(VIDEO)
 video.size(500,500)
+console.log("ML5 version",ml5.version)
+posenet=ml5.poseNet(video,modelloaded)
+posenet.on("pose",gotposes)
+}
+function gotposes(result){
+if(result.length>0){
+    console.log(result);
+    nosex=result[0].pose.nose.x
+    nosey=result[0].pose.nose.y
+    wristleftX=result[0].pose.leftWrist.x
+    wristleftY=result[0].pose.leftWrist.y
+    wristrightX=result[0].pose.rightWrist.x
+    wristrightY=result[0].pose.rightWrist.y
+
+
+
+}
+}
+function modelloaded(){
+console.log("Your model has been loaded")
 }
 function draw(){
-
+fill("#c8ff00")
+stroke("#c8ff00")
+square(100,100,80)
 }
 function preload(){}
